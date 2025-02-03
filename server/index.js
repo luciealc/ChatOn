4a11b0fd59453bcb924a6b612a5a31668f9eb202
Lucie Aloccio <114408320+luciealc@users.noreply.github.com>
2025-02-03 09:06:31 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Configuration Socket.IO
 const io = socketIo(server, {
   cors: {
-    origin: 'https://chat-on-chatroom.vercel.app/',
+    origin: 'https://chat-on-chatroom.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true
   }
